5aa6d09c9ef1a038ee06678a2c6054218226ae21
Randy <monyerandy15@gmail.com>
2022-06-14 23:25:05 +0100

    Basic Functions created
    Game logic and number of rounds (5) created

--- style.js
+++ style.js
@@ -17,11 +17,37 @@ function playRound(computerSelection, playerSelection) {
                (playerSelection === "paper" && computerSelection === 'rock')
             ) {
         return "You win!";
-    } else {
-        return "You lose";
+    } else if(    
+               (computerSelection === "rock" && playerSelection === "scissors") ||
+               (computerSelection === "scissors" && playerSelection === "paper") ||
+               (computerSelection === "paper" && playerSelection === "rock")
+            ) {
+        return "You lose!";
     }
 } 
 
-const playerSelection = 'papEr'.toLowerCase();
+const playerSelection = prompt("Rock, paper, scissors?").toLowerCase();
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
+
+
+// Play Game
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    // 5 rounds of game
+    for (let i = 0; i < 5; i++) { 
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result === "You win!") {
+            console.log(playerScore++)
+        } else if (result === "You lose") {
+            console.log(computerScore++)
+        } else {
+            console.log(result);
+        }
+    }
+    console.log(`Player: ${playerScore} Computer: ${computerScore}`);
+}
+game();
